e8c92dc40eaef892127f316960c0b3c2b8d836a5
Tatsuya Hashimura <luru84@gmail.com>
2025-09-18 17:57:26 +0900

    style: format with black/isort

--- src/yourpkg/__main__.py
+++ src/yourpkg/__main__.py
@@ -1,13 +1,14 @@
 import logging
 
 logging.basicConfig(level=logging.INFO, format="%(levelname)s %(message)s")
-log = logging.getLogger("yourpkg")   # ← これを入れる
+log = logging.getLogger("yourpkg")  # ← これを入れる
+
 
 def main():
     log.info("start")
     print("hello from yourpkg")
     log.info("done")
 
+
 if __name__ == "__main__":
     main()
-
